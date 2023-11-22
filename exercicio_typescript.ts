7c16af027bebf03bf4977ef21ce97d36f85ef6b4
Ramonrpro <ramonrpro@gmail.com>
2023-11-21 21:48:33 -0300

    :tada: commit  inicial

---
+++ exercicio_typescript.ts
@@ -0,0 +1,11 @@
+//FUNÇÃO MULTIPLICADORA
+function multiplicacao(valor1: number, valor2: number) {
+  return valor1 * valor2;
+}
+
+console.log(multiplicacao(10, 10));
+
+const saudacoes = (nome: string) =>
+  console.log(`Olá ${nome} é um prazer ter você por aqui`);
+
+saudacoes("Ramon");
